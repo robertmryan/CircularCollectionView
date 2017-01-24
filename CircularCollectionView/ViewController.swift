1b4f8a853da37aaf7156ed79d963bb809bd65d9d
Robert M. Ryan <robert.ryan@mindspring.com>
2017-01-24 12:38:56 -0800

    Initial Commit

---
+++ CircularCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircularCollectionView
+//
+//  Created by Robert Ryan on 1/24/17.
+//  Copyright © 2017 Robert Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
